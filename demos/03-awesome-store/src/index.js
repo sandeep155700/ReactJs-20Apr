b97236a53ef9ea687db12a86525dee8867e9d04e
Prashanth Puranik <puranik@digdeeper.in>
2019-03-18 04:55:16 +0530

    end of training

--- demos/03-awesome-store/src/index.js
+++ demos/03-awesome-store/src/index.js
@@ -1,9 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+class Counter extends React.Component {
+    constructor( props ) {
+        super( props );
 
-ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+        store.subscribe( this.forceUpdate.bind( this ) );
+    }
+
+    increment() {
+        store.dispatch( incrementAC() );
+    }
+    
+    decrement() {
+        store.dispatch( decrementAC() );
+    }
+
+    render() {
+        return (
+            <div>
+                {store.getState().counter}
+                <input type="number" />
+                <button onClick={this.increment.bind(this)}>+</button>
+                <button onClick={this.decrement.bind(this)}>-</button>
+            </div>
+        )
+    }
+}
+
+// action constants
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+// action creators
+const incrementAC = () => {
+    return {
+        type: INCREMENT,
+        payload: {
+            
+        }
+    };
+}
+
+const decrementAC = () => {
+    return {
+        type: DECREMENT
+    };
+}
+
+// Rules: Reducers are "pure functions"
+// 1. should not modify incoming inputs
+// 2. it should alwys return the same output for the same inputs (cannot make Ajax calls)
+// 3. It should not use any global variables and function
+const counterReducer = ( curState, action ) => {
+    let newState;
+
+    switch( action.type ) {
+        case INCREMENT:
+            newState = {
+                counter: curState.counter + 1
+            };
+            break;
+        case DECREMENT:
+            newState = {
+                counter: curState.counter - 1
+            };
+            break;
+        default:
+            newState = curState;
+    }
+
+    return newState;
+}
+
+const store = createStore(
+    counterReducer,
+    {
+        counter: 0
+    },
+    composeWithDevTools()
+);
+
+ReactDOM.render( <Counter counter={10} />, document.getElementById( 'root' ) );
