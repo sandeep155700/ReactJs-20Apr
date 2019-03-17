4dafc2de980dd2296f1859b68e5ff13f69fe1064
Prashanth Puranik <puranik@digdeeper.in>
2019-03-17 14:42:45 +0530

    started awesome store

---
+++ demos/03-awesome-store/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductCatalog from './ProductCatalog';
+
+ReactDOM.render(<ProductCatalog />, document.getElementById('root'));
